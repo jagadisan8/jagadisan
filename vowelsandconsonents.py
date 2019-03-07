b120cce714b6e0706bf869caaea3d4f286ef6208
jagadisan8 <47880595+jagadisan8@users.noreply.github.com>
2019-03-07 14:20:55 +0530

    Update vowelsandconsonents.py

--- vowelsandconsonents.py
+++ vowelsandconsonents.py
@@ -2,8 +2,8 @@ a=str(input())
 x=['a','e','i','o','u']
 y=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 if(a in x):
-    print("vowels")
+    print("Vowels")
 elif(a in y):
-    print("consonants")
+    print("Consonants")
 else:
     print("invalid")
